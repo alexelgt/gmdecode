e789c435f262585c443fc0406cfd5aa459b0d6f4
alexelgt <alexelgt@users.noreply.github.com>
2025-11-17 20:21:36 +0100

    Small changes and updated protos

--- run.py
+++ run.py
@@ -10,170 +10,169 @@ from sharedmodules.pokemon.regex import MOVE_GYMS_REGEX, FAMILY_REGEX, EXTENDED_
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-g", "--gamemaster_filename",
-                    help="Game Master file name")
+parser.add_argument('-g', '--gamemaster_filename', help='Game Master file name')
 
 args = parser.parse_args()
 
-RUN_FOLDER = os.path.dirname(__file__) + "/"
+RUN_FOLDER = os.path.dirname(__file__) + '/'
 
-proto_filename = "gamemaster.proto"
+proto_filename = 'gamemaster.proto'
 
-files_folder = f"{RUN_FOLDER}files"
-proto_folder = f"{files_folder}/protofiles"
-gamemaster_input_folder = f"{files_folder}/gamemasterinput"
-gamemaster_output_folder = f"{files_folder}/gamemasteroutput"
+files_folder = f'{RUN_FOLDER}files'
+proto_folder = f'{files_folder}/protofiles'
+gamemaster_input_folder = f'{files_folder}/gamemasterinput'
+gamemaster_output_folder = f'{files_folder}/gamemasteroutput'
 
-gamemaster_json_output_file = f"{RUN_FOLDER}GAME_MASTER.json"
+gamemaster_json_output_file = f'{RUN_FOLDER}GAME_MASTER.json'
 
-pythonfiles_folder = f"{RUN_FOLDER}pyproto"
+pythonfiles_folder = f'{RUN_FOLDER}pyproto'
 
-pogo_gm_protos_target = "POGOProtos.Rpc.DownloadGmTemplatesResponseProto"
-pogo_ga_protos_target = "POGOProtos.Rpc.AssetDigestOutProto"
+pogo_gm_protos_target = 'POGOProtos.Rpc.DownloadGmTemplatesResponseProto'
+pogo_ga_protos_target = 'POGOProtos.Rpc.AssetDigestOutProto'
 
 blocks_meta_info = [
     {
-        "block_name": "POKEMON_ID_BLOCK",
-        "block_elements": [],
-        "regex_find": EXTENDED_POKEMON_REGEX,
-        "string_find": None,
-        "element_first_level": "pokemonExtendedSettings",
-        "element_second_level": "uniqueId",
-        "enum_key_default_value": None,
-        "enum_key_prefix": None,
-        "enum_key_replace": [],
+        'block_name': 'POKEMON_ID_BLOCK',
+        'block_elements': [],
+        'regex_find': EXTENDED_POKEMON_REGEX,
+        'string_find': None,
+        'element_first_level': 'pokemonExtendedSettings',
+        'element_second_level': 'uniqueId',
+        'enum_key_default_value': None,
+        'enum_key_prefix': None,
+        'enum_key_replace': [],
     },
     {
-        "block_name": "FORM_BLOCK",
-        "block_elements": [],
-        "regex_find": EXTENDED_POKEMON_REGEX,
-        "string_find": None,
-        "element_first_level": "pokemonExtendedSettings",
-        "element_second_level": "form",
-        "enum_key_default_value": None,
-        "enum_key_prefix": None,
-        "enum_key_replace": [],
+        'block_name': 'FORM_BLOCK',
+        'block_elements': [],
+        'regex_find': EXTENDED_POKEMON_REGEX,
+        'string_find': None,
+        'element_first_level': 'pokemonExtendedSettings',
+        'element_second_level': 'form',
+        'enum_key_default_value': None,
+        'enum_key_prefix': None,
+        'enum_key_replace': [],
     },
     {
-        "block_name": "FAMILY_BLOCK",
-        "block_elements": [],
-        "regex_find": FAMILY_REGEX,
-        "string_find": None,
-        "element_first_level": "pokemonFamily",
-        "element_second_level": "familyId",
-        "enum_key_default_value": None,
-        "enum_key_prefix": "FAMILY_",
-        "enum_key_replace": [],
+        'block_name': 'FAMILY_BLOCK',
+        'block_elements': [],
+        'regex_find': FAMILY_REGEX,
+        'string_find': None,
+        'element_first_level': 'pokemonFamily',
+        'element_second_level': 'familyId',
+        'enum_key_default_value': None,
+        'enum_key_prefix': 'FAMILY_',
+        'enum_key_replace': [],
     },
     {
-        "block_name": "MOVES_BLOCK",
-        "block_elements": [],
-        "regex_find": MOVE_GYMS_REGEX,
-        "string_find": None,
-        "element_first_level": "moveSettings",
-        "element_second_level": "movementId",
-        "enum_key_default_value": None,
-        "enum_key_prefix": None,
-        "enum_key_replace": [],
+        'block_name': 'MOVES_BLOCK',
+        'block_elements': [],
+        'regex_find': MOVE_GYMS_REGEX,
+        'string_find': None,
+        'element_first_level': 'moveSettings',
+        'element_second_level': 'movementId',
+        'enum_key_default_value': None,
+        'enum_key_prefix': None,
+        'enum_key_replace': [],
     },
     {
-        "block_name": "BADGE_TYPE_BLOCK",
-        "block_elements": [],
-        "regex_find": None,
-        "string_find": "BADGE_",
-        "element_first_level": "badgeSettings",
-        "element_second_level": "badgeType",
-        "enum_key_default_value": None,
-        "enum_key_prefix": None,
-        "enum_key_replace": [],
+        'block_name': 'BADGE_TYPE_BLOCK',
+        'block_elements': [],
+        'regex_find': None,
+        'string_find': 'BADGE_',
+        'element_first_level': 'badgeSettings',
+        'element_second_level': 'badgeType',
+        'enum_key_default_value': None,
+        'enum_key_prefix': None,
+        'enum_key_replace': [],
     },
     {
-        "block_name": "BUDDY_ACTIVITY_BLOCK",
-        "block_elements": [],
-        "regex_find": None,
-        "string_find": "BUDDY_ACTIVITY_",
-        "element_first_level": "buddyActivitySettings",
-        "element_second_level": "activity",
-        "enum_key_default_value": None,
-        "enum_key_prefix": None,
-        "enum_key_replace": [],
+        'block_name': 'BUDDY_ACTIVITY_BLOCK',
+        'block_elements': [],
+        'regex_find': None,
+        'string_find': 'BUDDY_ACTIVITY_',
+        'element_first_level': 'buddyActivitySettings',
+        'element_second_level': 'activity',
+        'enum_key_default_value': None,
+        'enum_key_prefix': None,
+        'enum_key_replace': [],
     },
     {
-        "block_name": "BUDDY_CATEGORY_BLOCK",
-        "block_elements": [],
-        "regex_find": None,
-        "string_find": "BUDDY_CATEGORY_",
-        "element_first_level": "buddyActivityCategorySettings",
-        "element_second_level": "activityCategory",
-        "enum_key_default_value": None,
-        "enum_key_prefix": None,
-        "enum_key_replace": [],
+        'block_name': 'BUDDY_CATEGORY_BLOCK',
+        'block_elements': [],
+        'regex_find': None,
+        'string_find': 'BUDDY_CATEGORY_',
+        'element_first_level': 'buddyActivityCategorySettings',
+        'element_second_level': 'activityCategory',
+        'enum_key_default_value': None,
+        'enum_key_prefix': None,
+        'enum_key_replace': [],
     },
     {
-        "block_name": "QUEST_TYPE_BLOCK",
-        "block_elements": [],
-        "regex_find": None,
-        "string_find": "_EVOLUTION_QUEST",
-        "element_first_level": "evolutionQuestTemplate",
-        "element_second_level": "questType",
-        "enum_key_default_value": "???",
-        "enum_key_prefix": None,
-        "enum_key_replace": [],
+        'block_name': 'QUEST_TYPE_BLOCK',
+        'block_elements': [],
+        'regex_find': None,
+        'string_find': '_EVOLUTION_QUEST',
+        'element_first_level': 'evolutionQuestTemplate',
+        'element_second_level': 'questType',
+        'enum_key_default_value': '???',
+        'enum_key_prefix': None,
+        'enum_key_replace': [],
     },
     {
-        "block_name": "QUEST_TYPE_BLOCK",
-        "block_elements": [],
-        "regex_find": None,
-        "string_find": "QUEST_",
-        "element_first_level": "questSettings",
-        "element_second_level": "questType",
-        "enum_key_default_value": None,
-        "enum_key_prefix": None,
-        "enum_key_replace": [],
+        'block_name': 'QUEST_TYPE_BLOCK',
+        'block_elements': [],
+        'regex_find': None,
+        'string_find': 'QUEST_',
+        'element_first_level': 'questSettings',
+        'element_second_level': 'questType',
+        'enum_key_default_value': None,
+        'enum_key_prefix': None,
+        'enum_key_replace': [],
     },
     {
-        "block_name": "ITEM_ID_BLOCK",
-        "block_elements": [],
-        "regex_find": None,
-        "string_find": "ITEM_",
-        "element_first_level": "itemSettings",
-        "element_second_level": "itemId",
-        "enum_key_default_value": None,
-        "enum_key_prefix": None,
-        "enum_key_replace": [],
+        'block_name': 'ITEM_ID_BLOCK',
+        'block_elements': [],
+        'regex_find': None,
+        'string_find': 'ITEM_',
+        'element_first_level': 'itemSettings',
+        'element_second_level': 'itemId',
+        'enum_key_default_value': None,
+        'enum_key_prefix': None,
+        'enum_key_replace': [],
     },
     {
-        "block_name": "ITEM_TYPE_BLOCK",
-        "block_elements": [],
-        "regex_find": None,
-        "string_find": "ITEM_",
-        "element_first_level": "itemSettings",
-        "element_second_level": "itemType",
-        "enum_key_default_value": "???",
-        "enum_key_prefix": None,
-        "enum_key_replace": ["ITEM_", "ITEM_TYPE_"],
+        'block_name': 'ITEM_TYPE_BLOCK',
+        'block_elements': [],
+        'regex_find': None,
+        'string_find': 'ITEM_',
+        'element_first_level': 'itemSettings',
+        'element_second_level': 'itemType',
+        'enum_key_default_value': '???',
+        'enum_key_prefix': None,
+        'enum_key_replace': ['ITEM_', 'ITEM_TYPE_'],
     },
     {
-        "block_name": "ITEM_CATEGORY_BLOCK",
-        "block_elements": [],
-        "regex_find": None,
-        "string_find": "ITEM_",
-        "element_first_level": "itemSettings",
-        "element_second_level": "category",
-        "enum_key_default_value": "???",
-        "enum_key_prefix": None,
-        "enum_key_replace": ["ITEM_", "ITEM_CATEGORY_"],
+        'block_name': 'ITEM_CATEGORY_BLOCK',
+        'block_elements': [],
+        'regex_find': None,
+        'string_find': 'ITEM_',
+        'element_first_level': 'itemSettings',
+        'element_second_level': 'category',
+        'enum_key_default_value': '???',
+        'enum_key_prefix': None,
+        'enum_key_replace': ['ITEM_', 'ITEM_CATEGORY_'],
     },
     {
-        "block_name": "LOCATION_CARD_BLOCK",
-        "block_elements": [],
-        "regex_find": None,
-        "string_find": "LC_",
-        "element_first_level": "locationCardSettings",
-        "element_second_level": "locationCard",
-        "enum_key_default_value": None,
-        "enum_key_prefix": None,
-        "enum_key_replace": [],
+        'block_name': 'LOCATION_CARD_BLOCK',
+        'block_elements': [],
+        'regex_find': None,
+        'string_find': 'LC_',
+        'element_first_level': 'locationCardSettings',
+        'element_second_level': 'locationCard',
+        'enum_key_default_value': None,
+        'enum_key_prefix': None,
+        'enum_key_replace': [],
     },
 ]
 
@@ -194,10 +193,10 @@ blocks_meta_info = [
 
 def read_txt_file(path_to_file):
     try:
-        with open(path_to_file, 'r', encoding="utf-8") as txt_file:
+        with open(path_to_file, 'r', encoding='utf-8') as txt_file:
             txt_data = txt_file.read()
 
-            if txt_data == "":
+            if txt_data == '':
                 return None
             return txt_data
     except:
@@ -212,14 +211,18 @@ def main():
             gamemaster_filename = 'v2_GAME_MASTER'
 
         call(
-            f'protoc --proto_path="{proto_folder}" --python_out="{pythonfiles_folder}" --pyi_out="{pythonfiles_folder}" {proto_filename}', shell=True)
+            f'protoc --proto_path="{proto_folder}" --python_out="{pythonfiles_folder}" --pyi_out="{pythonfiles_folder}" {proto_filename}',
+            shell=True
+        )
 
-        proto_file = f"{proto_folder}/{proto_filename}"
+        proto_file = f'{proto_folder}/{proto_filename}'
 
-        gamemaster_output_file = f"{gamemaster_output_folder}/{gamemaster_filename}.txt"
+        gamemaster_output_file = f'{gamemaster_output_folder}/{gamemaster_filename}.txt'
 
         call(
-            f'protoc --proto_path="{proto_folder}" --decode {pogo_gm_protos_target} "{proto_file}" <"{gamemaster_input_folder}/{gamemaster_filename}"> "{gamemaster_output_file}"', shell=True)
+            f'protoc --proto_path="{proto_folder}" --decode {pogo_gm_protos_target} "{proto_file}" <"{gamemaster_input_folder}/{gamemaster_filename}"> "{gamemaster_output_file}"',
+            shell=True
+        )
 
         try:
             from pyproto.gamemaster_pb2 import DownloadGmTemplatesResponseProto  # pylint: disable=import-outside-toplevel, no-name-in-module
@@ -232,54 +235,50 @@ def main():
                 allow_unknown_field=True
             )
 
-            gamemaster_json_string = json_format.MessageToJson(
-                gamemaster_txt_parsed)
+            gamemaster_json_string = json_format.MessageToJson(gamemaster_txt_parsed)
 
             gamemaster_json = json.loads(gamemaster_json_string)
 
-            gamemaster_template_json = gamemaster_json["template"]
-            gamemaster_template_json.sort(key=lambda x: x["templateId"])
+            gamemaster_template_json = gamemaster_json['template']
+            gamemaster_template_json.sort(key=lambda x: x['templateId'])
 
-            print(f"batchId: {gamemaster_json['batchId']}")
-            print("-"*10)
+            print(f'batchId: {gamemaster_json["batchId"]}')
+            print('-'*10)
 
             try:
                 for element in gamemaster_template_json:
                     for blocks_meta_element in blocks_meta_info:
                         try:
-                            if blocks_meta_element["regex_find"] is not None:
+                            if blocks_meta_element['regex_find'] is not None:
                                 is_template_id = bool(
-                                    blocks_meta_element["regex_find"].search(element["templateId"]))
-                            elif blocks_meta_element["string_find"] is not None:
-                                is_template_id = blocks_meta_element["string_find"] in element["templateId"]
+                                    blocks_meta_element['regex_find'].search(element['templateId']))
+                            elif blocks_meta_element['string_find'] is not None:
+                                is_template_id = blocks_meta_element['string_find'] in element['templateId']
                             else:
                                 is_template_id = False
 
-                            if blocks_meta_element["block_name"] == "POKEMON_ID_BLOCK":
-                                if "form" in element["data"][blocks_meta_element["element_first_level"]]:
+                            if blocks_meta_element['block_name'] == 'POKEMON_ID_BLOCK':
+                                if 'form' in element['data'][blocks_meta_element['element_first_level']]:
                                     continue
 
                             if is_template_id and isinstance(
-                                    element["data"][blocks_meta_element["element_first_level"]][blocks_meta_element["element_second_level"]], int):
-                                if blocks_meta_element["enum_key_default_value"]:
-                                    enum_key = blocks_meta_element["enum_key_default_value"]
-                                elif blocks_meta_element["regex_find"] is not None:
-                                    _, enum_key = blocks_meta_element["regex_find"].search(
-                                        element["templateId"]).groups()
+                                    element['data'][blocks_meta_element['element_first_level']][blocks_meta_element['element_second_level']], int):
+                                if blocks_meta_element['enum_key_default_value']:
+                                    enum_key = blocks_meta_element['enum_key_default_value']
+                                elif blocks_meta_element['regex_find'] is not None:
+                                    _, enum_key = blocks_meta_element['regex_find'].search(element['templateId']).groups()
                                 else:
-                                    enum_key = element["templateId"]
+                                    enum_key = element['templateId']
 
-                                if blocks_meta_element["enum_key_prefix"]:
-                                    enum_key = blocks_meta_element["enum_key_prefix"] + enum_key
+                                if blocks_meta_element['enum_key_prefix']:
+                                    enum_key = blocks_meta_element['enum_key_prefix'] + enum_key
 
-                                if len(blocks_meta_element["enum_key_replace"]) == 2:
-                                    enum_key.replace(
-                                        *blocks_meta_element["enum_key_replace"])
+                                if len(blocks_meta_element['enum_key_replace']) == 2:
+                                    enum_key.replace(*blocks_meta_element['enum_key_replace'])
 
-                                enum_value = element["data"][blocks_meta_element["element_first_level"]
-                                                             ][blocks_meta_element["element_second_level"]]
+                                enum_value = element['data'][blocks_meta_element['element_first_level']][blocks_meta_element['element_second_level']]
 
-                                blocks_meta_element["block_elements"].append({
+                                blocks_meta_element['block_elements'].append({
                                     enum_key: enum_value
                                 })
                         except:
@@ -287,27 +286,27 @@ def main():
             except:
                 pass
 
-            with open(gamemaster_json_output_file, 'w', encoding="utf-8") as f:
+            with open(gamemaster_json_output_file, 'w', encoding='utf-8') as f:
                 json.dump(gamemaster_template_json, f, indent=4)
 
             blocks_missing_enums_info = [{
-                "block_name": blocks_meta_element["block_name"],
-                "block_elements": [dict(t) for t in {tuple(d.items()) for d in blocks_meta_element["block_elements"]}]
-            } for blocks_meta_element in blocks_meta_info if len(blocks_meta_element["block_elements"]) > 0]
+                'block_name': blocks_meta_element['block_name'],
+                'block_elements': [dict(t) for t in {tuple(d.items()) for d in blocks_meta_element['block_elements']}]
+            } for blocks_meta_element in blocks_meta_info if len(blocks_meta_element['block_elements']) > 0]
 
             for blocks_missing_enums_element in blocks_missing_enums_info:
-                print(blocks_missing_enums_element["block_name"])
+                print(blocks_missing_enums_element['block_name'])
 
-                for block_element in blocks_missing_enums_element["block_elements"]:
+                for block_element in blocks_missing_enums_element['block_elements']:
                     for enum_key, enum_value in block_element.items():
                         print(f'{enum_key} = {enum_value};')
 
-                print("-"*10)
+                print('-'*10)
         except:
             pass
     except:
         pass
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
